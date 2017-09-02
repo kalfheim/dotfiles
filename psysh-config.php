d1c36b22d2b4fa0ed616df1196da8d427640e6e7
Kristoffer Alfheim <kristoffer.alfh@gmail.com>
2017-09-02 20:00:48 +0200

    is now built-in

--- psysh-config.php
+++ psysh-config.php
@@ -13,31 +13,6 @@ if ($is->composer && ! $is->composerInstalled) {
 }
 
 if ($is->laravel) {
-    // Alias Eloquent models for easy access.
-    // @see https://gist.github.com/calebporzio/cdf70bd390688646fda65490006eb0a6
-
-    $finder = (new Symfony\Component\Finder\Finder)->files()->name('*.php')->in(base_path().'/app');
-
-    foreach ($finder as $file) {
-        $namespace = app()->getNamespace();
-
-        if ($relativePath = $file->getRelativePath()) {
-            $namespace .= strtr($relativePath, '/', '\\') . '\\';
-        }
-
-        $class = $namespace . $file->getBasename('.php');
-
-        try {
-            $r = new ReflectionClass($class);
-
-            if ($r->isSubclassOf('Illuminate\\Database\\Eloquent\\Model')) {
-                class_alias($class, $file->getBasename('.php'));
-            }
-        } catch (Exception $e) {
-            //
-        }
-    }
-
     // Print SQL queries made by the query builder (including Eloquent)
     DB::listen(function (Illuminate\Database\Events\QueryExecuted $query) {
         printf("[%s] %s\n", $query->connectionName, $query->sql);
